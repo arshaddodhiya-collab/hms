6457ecc43e6710d04bbb4b8f728e3439f4a94d0d
arshaddodhiya-collab <arshad.dodhiya@artemhealthtech.com>
2026-01-16 14:38:02 +0530

    apply primeng and primeflex in product-search module

--- src/app/product-search/product-search.module.ts
+++ src/app/product-search/product-search.module.ts
@@ -4,6 +4,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ProductSearchRoutingModule } from './product-search-routing.module';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { ProductSearchComponent } from './components/product-search/product-search.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
@@ -16,6 +18,8 @@ import { ProductListComponent } from './components/product-list/product-list.com
     ProductSearchRoutingModule,
     TableModule,
     ButtonModule,
+    CardModule,
+    InputTextModule,
   ],
 })
 export class ProductSearchModule {}
